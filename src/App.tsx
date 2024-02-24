138d65a68dc7dce2d4757177678bbd49833918b0
Kushal Shah <60709835+shahkushal38@users.noreply.github.com>
2024-02-24 17:50:42 +0530

    refactoring and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { FirstComponent } from "./components";
 import { ProductTable } from "./components/ProductTable/ProductTable";
-import { BrowserRouter, Route, Routes, redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./components/Login/Login";
 import TodoList from "./components/TodoList/TodoList";
 import { useToken } from "./components/useToken/useToken";
 
 function App() {
   const { getToken, saveToken } = useToken();
-  // const navigate = useNavigate();
 
   // useEffect(() => {
   //   if (!getToken()) {
@@ -19,7 +18,6 @@ function App() {
   // }, [getToken]);
 
   if (!getToken()) {
-    console.log("In Get App");
     return <Login setToken={saveToken} />;
   }
 
@@ -28,7 +26,7 @@ function App() {
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Login setToken={saveToken} />} /> */}
+          <Route path="/" element={<ProductTable />} />
           <Route path="/FirstComponent" element={<FirstComponent />} />
           <Route path="/productTable" element={<ProductTable />} />
           <Route path="/todoList" element={<TodoList />} />
